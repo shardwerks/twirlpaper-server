8704e4c3effc83889c126720e77a9aa20ef205c4
shard <none@none>
2009-07-13 16:52:00 -0700

    Remove elgg, move to Coppermine as gallery server software

--- elgg/mod/embed/views/default/embed/metatags.php
+++
@@ -1,6 +0,0 @@
-		<script type="text/javascript">
-		 jQuery(document).ready(function($) {
-		  $('a[rel*=facebox]').facebox()
-		});
-		</script>
-		<script type="text/javascript" src="<?php echo $vars['url']; ?>mod/embed/jquery.form.js"></script> 
