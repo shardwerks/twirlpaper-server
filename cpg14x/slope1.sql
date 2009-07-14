dccb6ef2d3fb933b6a655f79576e881c0ab0a78e
shard <none@none>
2009-07-14 12:33:26 -0700

    Subsumed slope one table creation sql into schema.sql

--- cpg14x/slope1.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE slope1 (
-	pid1 int(11) NOT NULL default '0',
-	pid2 int(11) NOT NULL default '0',
-	votes int(11) NOT NULL default '0',
-	sum int(11) NOT NULL default '0',
-	PRIMARY KEY (pid1, pid2)
-) TYPE=MyISAM COMMENT='Slope one calculation matrix';
