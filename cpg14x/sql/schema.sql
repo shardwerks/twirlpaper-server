858d12b2de6114d3eccb79dd177a7e3895ebcbe8
shard <none@none>
2009-07-14 12:32:07 -0700

    Update schema with slope one and nonce table for auto install

--- cpg14x/sql/schema.sql
+++ cpg14x/sql/schema.sql
@@ -354,3 +354,15 @@ CREATE TABLE `CPG_hit_stats` (
   PRIMARY KEY  (`sid`)
 ) TYPE = MyISAM COMMENT='Detailed stats about hits, only used when enabled';
 # --------------------------------------------------------
+
+CREATE TABLE slope1 (
+	pid1 int(11) NOT NULL default '0',
+	pid2 int(11) NOT NULL default '0',
+	votes int(11) NOT NULL default '0',
+	sum int(11) NOT NULL default '0',
+	PRIMARY KEY (pid1, pid2)
+) TYPE=MyISAM COMMENT='Slope one calculation matrix';
+
+CREATE TABLE nonce (
+	`valid` CHAR( 40 ) NOT NULL
+) TYPE=MyISAM COMMENT='Client login nonce';
