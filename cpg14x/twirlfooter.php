578d56847a97c4508201b965b0f5dd9b0975655a
shard <none@none>
2010-03-30 15:30:14 -0700

    Minify robots.txt, add footer to all pages, adjust CSS to include footer

---
+++ cpg14x/twirlfooter.php
@@ -0,0 +1,23 @@
+  <div id="copyright">
+    <ul>
+      <li><a href="twirlprivacy.php">Privacy Policy</a></li>
+      <li><a href="twirltos.php">Terms of Service</a></li>
+	  <li>
+		<!-- AddThis Button BEGIN -->
+		<a class="addthis_button" href="http://www.addthis.com/bookmark.php?v=250&amp;username=leoengine">
+		<img src="http://s7.addthis.com/static/btn/v2/lg-bookmark-en.gif" width="125" height="16" alt="Bookmark and Share" style="border:0"/></a>
+		<!-- AddThis Button END -->
+	  </li>
+      <li class="last">Copyright &copy; 2010, <a href="http://www.leoengine.com/index.php">LeoEngine</a>.  All rights reserved.</li>
+    </ul>
+  </div>
+<!-- Delay Javascript loading to delay blocking -->
+<script type="text/javascript" src="http://www.google.com/cse/brand?form=cse-search-box&lang=en"></script>
+<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#username=leoengine"></script>
+<script type="text/javascript" src="http://www.google-analytics.com/ga.js"></script>
+<script type="text/javascript">
+	try {
+		var pageTracker = _gat._getTracker("UA-13119359-1");
+		pageTracker._trackPageview();
+	} catch(err) {}
+</script>
